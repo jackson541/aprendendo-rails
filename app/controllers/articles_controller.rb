17571399a4d308bd738a0c0ebb8a7c2eb9d15776
JacksonAlves123 <jacksonalves541@gmail.com>
2020-08-26 11:04:00 -0300

    adiciona autenticação básica aos métodos

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+    http_basic_authenticate_with name: "jackson", password: "senha", except: [:index, :show]
+
     def index
         @articles = Article.all
     end
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,6 @@
 class CommentsController < ApplicationController
+    http_basic_authenticate_with name:'jackson', password: 'senha', only: :destroy
+
     def create
         @article = Article.find(params[:article_id])
         @comment = @article.comments.create(comments_params)
